bdd9cce3afefb3eada5d4f0bbf410467496f8b86
[Kushantha Charuka] <[kushiyaz123@gmail.com]>
2019-07-24 17:13:07 +0530

    Version 2 updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,6 +9,8 @@ import { WeddingsComponent } from './weddings/weddings.component';
 import { OffersComponent } from './offers/offers.component';
 import { SigninComponent } from './auth/signin/signin.component';
 import { SignupComponent } from './auth/signup/signup.component';
+import { HomeComponent } from './home/home.component';
+
 
 const routes: Routes = [
     { path: 'accomodation', component: AccomodationComponent },
@@ -19,6 +21,7 @@ const routes: Routes = [
     { path: 'offers', component: OffersComponent },
     { path: 'signin', component: SigninComponent },
     { path: 'signup', component: SignupComponent },
+    { path: '', component: HomeComponent },
 ];
 
 @NgModule({
